819fc6eb43a4ab92bb6426ecba7f6d74a41dafa7
aslı kılıç <aslhanklnc@gmail.com>
2025-12-11 19:58:40 +0300

    Mini.UI login finish

--- Mini.UI/Controllers/HomeController.cs
+++ Mini.UI/Controllers/HomeController.cs
@@ -1,21 +1,47 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Mini.AuthApi.Models.Dto;
+using Mini.UI.IContract;
 using Mini.UI.Models;
+using Mini.UI.Models.Dto;
+using Mini.UI.Services;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
+using System.Security.Claims;
 
 namespace Mini.UI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> _logger, IAuthService _auth, ITokenService _token) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
-            return View();
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto responseDto = await _auth.Login(obj);
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto =
+                    JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+                SigInUser(loginResponseDto);
+                _token.SetToken(loginResponseDto.Token);
+                return RedirectToAction("Privacy", "Home");
+            }
+            else
+            {
+                TempData["error"] = responseDto.Message;
+                return View(obj);
+            }
         }
 
         public IActionResult Register()
@@ -27,6 +53,33 @@ namespace Mini.UI.Controllers
         {
             return View();
         }
-      
+        //cookie tabanlı oturum açmasılması
+        private async Task SigInUser(LoginResponseDto model)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            //Token decode ediliyor
+            var jwt = handler.ReadJwtToken(model.Token);
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Email,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Sub).Value));
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Name,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Name).Value));
+
+            //Roller ekleniyor
+            identity.AddClaim(new Claim(ClaimTypes.Name,
+               jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+            identity.AddClaim(new Claim(ClaimTypes.Role,
+                jwt.Claims.FirstOrDefault(u => u.Type == "role").Value));
+
+            //Kullanıcı “principal” oluşturuluyor
+            //ClaimsPrincipal, kimlik bilgilerini (identity) ve claim’leri taşıyan nesnedir
+            var principal = new ClaimsPrincipal(identity);
+            //Oturum açma işlemi gerçekleştiriliyor
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+        }
     }
 }
--- Mini.UI/Program.cs
+++ Mini.UI/Program.cs
@@ -1,8 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Mini.HttpServiceBus.Services;
+using Mini.UI.IContract;
+using Mini.UI.Services;
+using Mini.UI.Utility;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//AddHttpContextAccessor
+//TokenService için gerekli
+//Baþlýklar, çerezler, sorgu parametreleri ve kullanýcý talepleri gibi HTTP istek
+//ve yanýtýnýn çeþitli yönlerine eriþmenizi saðlar
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
 
+
+
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+StaticBase.AuthApiBase = builder.Configuration["ServicesUrls:AuthApiUrl"];
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromHours(10);
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Auth/AccessDenied";
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +42,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
